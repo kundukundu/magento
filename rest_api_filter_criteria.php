ea10c14e0e34ab3265a988c4343ea553a8537365
Chiyana Simões <csimoes@inusual.com>
2018-02-21 15:32:17 +0100

    Update rest_api_filter_criteria.php

--- rest_api_filter_criteria.php
+++ rest_api_filter_criteria.php
@@ -1,12 +1,11 @@
 <?php
   
-        // $filtro =  'filtro%';
+        // $filtro =  'filtro%'; sku like...
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['field']='sku';
-        query['searchCriteria']['filter_groups'][1]['filters'][0]['value']= str_replace('%', '%25', $filtro);
+        $query['searchCriteria']['filter_groups'][1]['filters'][0]['value']= str_replace('%', '%25', $filtro);
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['conditionType']= 'like';
   
-        // $sku1..$sku1000
-  
+        // $sku1..$sku1000; sku between
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['field']='sku';
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['value']= $sku1;
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['conditionType']= 'from';
@@ -14,8 +13,7 @@
         $query['searchCriteria']['filter_groups'][2]['filters'][0]['value']= $sku1000;
         $query['searchCriteria']['filter_groups'][2]['filters'][0]['conditionType']= 'to';
         
-        // $filtro = "sku1,sku2";
-        
+        // $filtro = "sku1,sku2"; sku in
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['field']='sku';
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['value']= $filtro;
         $query['searchCriteria']['filter_groups'][1]['filters'][0]['conditionType']= 'in';
